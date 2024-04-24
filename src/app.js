04f2fa0c4f6f81d3e735f38db80eeda56fe80c4c
Sang Han <sanghan@protonmail.com>
2024-04-24 13:11:37 -0700

    first commit

---
+++ src/app.js
@@ -0,0 +1,125 @@
+import * as css from "./style.css";
+import screenfull from 'screenfull';
+import {
+  GestureRecognizer,
+  FilesetResolver,
+  DrawingUtils,
+} from "@mediapipe/tasks-vision";
+
+// Create task for image file processing:
+const vision = await FilesetResolver.forVisionTasks(
+  "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm",
+);
+
+const gestureRecognizer = await GestureRecognizer.createFromOptions(vision, {
+  baseOptions: {
+    modelAssetPath: "https://storage.googleapis.com/mediapipe-tasks/gesture_recognizer/gesture_recognizer.task",
+    delegate: "GPU"
+  },
+  numHands: 2,
+  runningMode: 'VIDEO',
+});
+
+const video = document.getElementById("webcam");
+const canvasElement = document.getElementById("output_canvas");
+const canvasCtx = canvasElement.getContext("2d");
+const webcamButton = document.getElementById("webcamButton");
+const fullscreenButton = document.getElementById('fullscreen-button');
+
+let lastVideoTime = -1;
+let results = undefined;
+
+webcamButton.addEventListener("click", () => {
+  if (!gestureRecognizer) {
+    alert("Please wait for gestureRecognizer to load");
+    return;
+  }
+
+  let selectedCamera = document.querySelector('input[name="radio"]:checked').value;
+  const constraints = {
+    video: {
+      width: {
+        min: 640,
+        ideal: 1280
+      },
+      height: {
+        min: 480,
+        ideal: 720
+      },
+      facingMode: selectedCamera,
+    }
+  };
+  // Activate the webcam stream.
+  navigator.mediaDevices.getUserMedia(constraints).then(stream => {
+    let settings = stream.getVideoTracks()[0].getSettings(); 
+    let inputs = document.querySelector('.inputs');
+
+    video.srcObject = stream;
+    video.addEventListener("loadeddata", predictWebcam);
+
+    canvasElement.width = settings.width;
+    canvasElement.height = settings.height;
+
+    video.play();
+
+    canvasElement.style.display = 'block';
+    inputs.style.display = "none";
+  });
+});
+
+async function predictWebcam() {
+  if (video.currentTime !== lastVideoTime) {
+    lastVideoTime = video.currentTime;
+    results = gestureRecognizer.recognizeForVideo(video, Date.now());
+  }
+
+  canvasCtx.save();
+  canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+  canvasCtx.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
+
+  const drawingUtils = new DrawingUtils(canvasCtx);
+
+  if (results.landmarks) {
+    for (const landmarks of results.landmarks) {
+      drawingUtils.drawConnectors(
+        landmarks,
+        GestureRecognizer.HAND_CONNECTIONS,
+        {
+          color: "#00FF00",
+          lineWidth: 5
+        }
+      );
+      drawingUtils.drawLandmarks(landmarks, {
+        color: "#FF0000",
+        lineWidth: 2
+      });
+    }
+  }
+
+  canvasCtx.restore();
+  if (results.gestures.length > 0) {
+    const categoryName = results.gestures[0][0].categoryName;
+    const categoryScore = parseFloat(results.gestures[0][0].score * 100).toFixed(2);
+    const handedness = results.handednesses[0][0].displayName;
+    const text = `GestureRecognizer: ${categoryName}, Confidence: ${categoryScore} %, Handedness: ${handedness}`;
+
+    canvasCtx.font = "16px serif";
+    canvasCtx.textAlign = 'center';
+    canvasCtx.fillStyle = 'blue';
+    canvasCtx.fillText(text, canvasElement.width / 2, 20);
+  }
+
+  window.requestAnimationFrame(predictWebcam);
+}
+
+fullscreenButton.addEventListener('click', () => {
+    if (screenfull.isEnabled) {
+        screenfull.toggle(canvasElement, {navigationUI: 'hide'});
+    }
+});
+
+document.addEventListener('click', () => {
+    if (screenfull.isEnabled && screenfull.isFullscreen) {
+        screenfull.exit();
+    }
+});
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/app.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      { test: /\.html$/, use: 'html-loader' }
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 8000
+  },
+  performance: {
+    hints: false
+  }
+};
